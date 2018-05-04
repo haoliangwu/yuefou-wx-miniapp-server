7241f31e3fe754496a3a876aa4bb9e3eec3c9c51
Lyon <wuhaoliang@cloudist.cc>
2018-05-04 17:37:27 +0800

    [IMP] 后端同样会对 token 是否超时做校验

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -1,6 +1,21 @@
 const request = require('request-promise-native')
 
+let cache = {
+    templateMsgToken: null
+}
+
 async function templateMsgToken (ctx, next) {
+    if (cache.templateMsgToken) {
+        const token = JSON.parse(cache.templateMsgToken)
+        const { expires_in, timestamp } = token
+        const now = Number.parseInt(Date.now() / 1000)
+
+        if (now < timestamp + expires_in) {
+            ctx.state.data = token
+            return
+        }
+    }
+
     const url = 'https://api.weixin.qq.com/cgi-bin/token'
     const qs = {
         grant_type: 'client_credential',
@@ -16,10 +31,11 @@ async function templateMsgToken (ctx, next) {
     })
 
     if (result.errcode) {
-        ctx.throw(400, {errors: result})
+        ctx.throw(400, { errors: result })
     } else {
         result.timestamp = Number.parseInt(Date.now() / 1000)
 
+        cache.templateMsgToken = JSON.stringify(result)
         ctx.state.data = result
     }
 }
