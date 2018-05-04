5df5d157dea02672f15d4acbdddc2dc83ff4674f
Lyon <wuhaoliang@cloudist.cc>
2018-05-04 14:51:52 +0800

    [IMP] 更改 graphql endpoint

--- controllers/graphql.js
+++ controllers/graphql.js
@@ -7,7 +7,7 @@ const gql = require('graphql-tag')
 const fetch = require('node-fetch')
 // const debug = require('debug')('graphql-route')
 
-const GRAPHQL_ENDPOINT = 'http://yuefou_graphql.littlelyon.com'
+const GRAPHQL_ENDPOINT = 'http://yuefou_dev.littlelyon.com/graphql'
 // const GRAPHQL_ENDPOINT = 'http://58.87.91.173:3000'
 
 const link = new HttpLink({
